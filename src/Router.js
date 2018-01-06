ec3f7a34b26b60d1ba367a14738deaf34ef2fa86
Alysson Narloch <alysson.narloch@gmail.com>
2018-01-06 19:27:08 -0200

    Criação da tela de novo funcionário e navegação.

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Actions, Router, Scene, Stack } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
     return (
-        <Router sceneStyle={{ paddingTop: 65 }}>
-            <Scene key="login" component={LoginForm} title="Please Login" />
-            <Scene key="employeeList" component={EmployeeList} title="Employee list" />
+        <Router>
+            <Stack key="root" hideNavBar>
+                <Scene key="auth">
+                    <Scene key="login" component={LoginForm} title="Please Login" />
+                </Scene>
+
+                <Scene key="main">
+                    <Scene 
+                        onRight={() => Actions.employeeCreate()}
+                        rightTitle="Add"
+                        key="employeeList" 
+                        component={EmployeeList} 
+                        title="Employee list" 
+                        initial
+                    />
+                    <Scene key="employeeCreate" component={EmployeeCreate} title="New Employee" />
+                </Scene>
+            </Stack>
         </Router>
     );
 };
